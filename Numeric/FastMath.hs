b443caa248227914b4297c3918289733900015b6
Liyang HU <git@liyang.hu>
2014-05-30 10:43:36 +0900

    Numeric.FastMath: drop # suffix for variables; consistentify indenting

--- Numeric/FastMath.hs
+++ Numeric/FastMath.hs
@@ -14,12 +14,12 @@ module Numeric.FastMath () where
 import GHC.Exts
 
 {-# RULES
-"minusFloat x x"    forall x#. minusFloat#  x#   x#   = 0.0#
-"timesFloat x 0.0"  forall x#. timesFloat#  x#   0.0# = 0.0#
-"timesFloat 0.0 x"  forall x#. timesFloat#  0.0# x#   = 0.0#
+"minusFloat x x"    forall x. minusFloat#  x    x       = 0.0#
+"timesFloat x 0"    forall x. timesFloat#  x    0.0#    = 0.0#
+"timesFloat 0 x"    forall x. timesFloat#  0.0# x       = 0.0#
 
-"minusDouble x x"    forall x#. (-##) x#    x#    = 0.0##
-"timesDouble 0.0 x"  forall x#. (*##) 0.0## x#    = 0.0##
-"timesDouble x 0.0"  forall x#. (*##) x#    0.0## = 0.0##
+"minusDouble x x"   forall x. (-##) x       x       = 0.0##
+"timesDouble 0 x"   forall x. (*##) 0.0##   x       = 0.0##
+"timesDouble x 0"   forall x. (*##) x       0.0##   = 0.0##
   #-}
 
--- Numeric/FastMath/Infinitesimal.hs
+++ Numeric/FastMath/Infinitesimal.hs
@@ -1,11 +1,12 @@
--- | Also rewrite @0/x@ to @+0@, which should really be @-0@ for negative @x@.
+-- | Also rewrite @0/x@ to @+0@, which should really be @-0@ for negative
+-- @x@, and @NaN@ when @x@ = 0.
 
 module Numeric.FastMath.Infinitesimal () where
 
 import GHC.Exts
 
 {-# RULES
-"divideFloat 0.0 x" forall x#. divideFloat# 0.0# x#   = 0.0#
-"divideDouble 0.0 x" forall x#. (/##) 0.0## x#    = 0.0##
+"divideFloat 0 x"   forall x. divideFloat#  0.0#    x   = 0.0#
+"divideDouble 0 x"  forall x. (/##)         0.0##   x   = 0.0##
   #-}
 
